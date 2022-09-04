2907757802fa6c0873374862043028d82dfd79eb
zgold33 <zgold33@gmail.com>
2022-09-04 18:45:24 +0300

    Initial Commit

---
+++ DeliveryFoody/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeliveryFoody
+//
+//  Created by Сергей Золотухин on 04.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
